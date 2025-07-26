505943dc8bd7bd0db9ae35fa06590b7fa6c75386
NGdev2 <aidar.nizamov141@gmail.com>
2025-07-27 01:14:18 +0200

    d1

---
+++ d1/main.py
@@ -0,0 +1,13 @@
+print("""Aidar
+July 27, 2025
+
+I am signing up for Replit's 100 Days of Python challenge!🤪
+
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.👻
+
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field! (🤡)
+
+I am feeling 🙃
+
+You can follow my progress at replit.com/@aidarnizamov141
+""")
